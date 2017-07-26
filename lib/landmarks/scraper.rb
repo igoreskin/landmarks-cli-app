d8e1f1a7b1114abc27761e04035284a53718060c
Igor Eskin <eskinhd@gmail.com>
2017-07-26 17:47:58 +0000

    Formatting the output of #print_landmarks_index method

--- lib/landmarks/scraper.rb
+++ lib/landmarks/scraper.rb
@@ -17,25 +17,49 @@ class Landmarks::Scraper
     puts self.get_page.css("h1").text
   end
 
+=begin
   def print_landmarks_index
-    puts Landmarks::Landmark.all.each.with_index { |el, index| puts "#{index+1}.", "\n\n#{el.name}".strip }
+    list = []
+    Landmarks::Landmark.all.each { |el| list << "#{el.name}".gsub(/^\s*/, '') }
+    list
+    list.each.with_index { |el, index| puts "#{el}".strip }
     nil
   end
 
-  def get_landmark_page(url)
+
+  def print_landmarks_index
+    Landmarks::Landmark.all.each.with_index { |el, index| puts "#{index+1}.".strip, "#{el.name}".strip } #.gsub("u00A0", "\t") }
+    nil
+  end
+=end
+
+  def print_landmarks_index
+    Landmarks::Landmark.all.each.with_index { |el, index| puts "#{index+1}. #{el.name}".gsub("\r\n", "") }
+    nil
+  end
+
+
+
+  def print_landmarks_urls
+    Landmarks::Landmark.all.each.with_index { |el, index| puts "#{index+1}.\t#{el.url}" }
+    nil
+  end
+
+
+def get_landmark_page(url)
     Nokogiri::HTML(open(url))
   end
 
   def scrape_landmark_name(url)
-  	puts self.get_landmark_page(url).css("h1").text
+  	self.get_landmark_page(url).css("h1").text
   end
 
   def scrape_landmark_description(url)
-    puts self.get_landmark_page(url).css("p").text
+    self.get_landmark_page(url).css("p").text
   end
 
   def scrape_landmark_directions(url)
-    puts self.get_landmark_page(url).css(".prop-list").text
+    self.get_landmark_page(url).css(".prop-list").text
   end
 
 
